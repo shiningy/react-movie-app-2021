73b9fcdd3a6cf8c2330504a2d454e086f6c3078a
myunghyun_yang <shiningy84@gmail.com>
2021-01-17 12:33:56 +0900

    work well

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>
+        About this page: I built it because I love movies
+      </span>
+    </div>
+  );
+}
+
+export default About;
